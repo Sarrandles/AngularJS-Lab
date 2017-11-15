3526f14752462ebcd066dc7093c9343cdd497e28
Sarah Randles <sarrandles@gmail.com>
2017-11-15 09:21:32 -0500

    fixed problem with the button

--- controller.js
+++ controller.js
@@ -1,6 +1,7 @@
 (function() {
   function FormController() {
     var vm = this;
+    
     vm.toDoList = ["Walk Syntax", "Do the laundry", "Eat all the tacos", "Git Gud", "Buy milk", "Bake a pie"];
 
     vm.addItem = function(index) {
@@ -14,4 +15,7 @@
   angular
     .module("moduleApp")
     .controller("FormController", FormController);
+
+    //first parameter is name of our controller. the second parameter is the name of the function we want
+    // to have access to... it's easier to keep the same name for both
 })();
--- module.js
+++ module.js
@@ -2,5 +2,12 @@
 
   angular
     .module("moduleApp", []);
+    //first parameter is name of the module and the 2nd is an array (dependencies the module may need to use)
+    //to use it elsewhere you use:
+    // angular
+    // .module("name")
+
+    //ng-repeat will create a new element for each thing we target and want to repeat
+    //filters help you sort through content.
 
 })();
